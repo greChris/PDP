547ea01e2a3c5ac8470d96c1429d7030f2ae116b
Koledrianos Xristos <chris@Chris>
2024-12-20 20:17:12 +0200

    asgrehijolj

--- finding.c
+++ finding.c
@@ -1,11 +1,39 @@
-/* USER: user5.37pdp
-LANG: C
-TASK: hiring*/
-#include <stdio.h>
-#include "findinglib.h"//to arxio tha mas to dosoun 30 tou mina
-
-
-int main(void)
-{
-
-}
+    /* USER: user5.37pdp 
+    LANG: C 
+    TASK: finding*/  
+    #include "findinglib.h"  //xriazete findinglib.h arxio 
+    #include <stdio.h>  
+      
+    int main() {  
+      int N, D, x1, x2, q ,A,B;  
+      int subtask_num = getSubtask();  
+      
+      while (true) {  
+      
+        D = getD();  
+        q = query(1);  
+        x1 = q;  
+          
+        if(D != -1)  
+        {  
+            A = (x1 - D) / 2 + 1;  
+            B = A + D;  
+            printf("%c %c",A,B);  
+            if (!answer(A, B)) break;  
+            else  
+            {  
+                continue;  
+            }  
+        }  
+      
+        q = query(x1 / 2 + 1);  
+        x2 = q;  
+          
+        A = (x1 - x2) / 2 + 1;  
+        B = A + x2;  
+        printf("%c %c",A,B);  
+        if (!answer(A, B)) break;  
+      }  
+      
+      return 0;  
+    }  
